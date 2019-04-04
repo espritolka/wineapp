3c2818720a46ae0f37939f6b22011d0f8c73c1ab
Iugai Olga <espritolka@gmail.com>
2019-04-04 23:55:18 +0300

    add connect db

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require('path')
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const passport = require('passport')
+const session = require('express-session')
+const RedisStore = require('connect-redis')(session)
 const jsonParser = express.json();
 const fs = require('fs');
+
 var port = 3000;
 app.listen(port);
 
@@ -15,14 +24,35 @@ app.use(function (request, response, next) {
     fs.appendFile("server.log", data + "\n", function () { });
     next();
 });
-app.use(function(req, res, next){ 
-  //auth
-    next();
-});
-app.get('/api/users', function(req, res){
+app.use(bodyParser.urlencoded({
+    extended: false
+  }))
+  
+  require('./auth').init(app)
+  redisStore = {
+    url: 'redis://localhost',
+    secret: 'my-strong-secret'
+  }
+  app.use(session({
+    store: new RedisStore({
+      url: redisStore.url
+    }),
+    secret: redisStore.secret,
+    resave: false,
+    saveUninitialized: false
+  }))
+  
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+app.get('/', function(req,res){
+res.send("MAIN")
+})
+
+app.get('/api/users',  passport.authenticationMiddleware(),function(req, res){
     res.send("UsersList")
 })
-app.route('/api/users/:id')
+app.route('/api/users/:id',  passport.authenticationMiddleware())
     .get(function(req,res){
         var id = req.params.id
         res.send(id);
@@ -38,7 +68,7 @@ app.route('/api/users/:id')
     });
 app.get('/api/wines', function(req, res){
     res.send("WinesList")
-})   
+});
 app.route('/api/wines/:id')
     .get(function(req,res){
         var id = req.params.id
@@ -68,4 +98,39 @@ app.route('/api/reiting')
     })
     .delete(function(req,res){
         res.send("i'm delete");
-    })       
+    })     
+    
+    var UserSchema = new mongoose.Schema({
+        username: {
+          type: String,
+          unique: true,
+          required: true
+        },
+        password: {
+          type: String,
+          required: true
+        }
+      });
+      mongoose.model('user', UserSchema);
+      mongoose.connect("mongodb://localhost:27017/wineappsdb", { useNewUrlParser: true }, function(err){
+    if(err) return console.log(err);
+    // app.listen(3000, function(){
+    //     console.log("Сервер ожидает подключения...");
+    // });
+});
+    app.post('/login', passport.authenticate('local', {
+        successRedirect: '/api/wines',
+        failureRedirect: '/'
+      }))  
+    app.post('/register', function(req,res){
+        var user = new User({ username: req.body.email, password: req.body.password});
+    user.save(function(err) {
+      return err
+        ? next(err)
+        : req.logIn(user, function(err) {
+          return err
+            ? next(err)
+            : res.redirect('/private');
+        });
+    });
+})   
